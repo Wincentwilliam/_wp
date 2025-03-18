838eaf1df29c49780ec3cca02a1a082ed2e2886a
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-19 00:43:19 +0800

    commit

---
+++ homework/Homework1.js
@@ -0,0 +1,3 @@
+let x ="42";
+let result = parseInt(x) +8;
+console.log(result);
---
+++ homework/Homework2.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 20;
+
+if (x > y){
+    console.log("x is greater than y");
+}else{
+    console.log("x is less than y");
+}
