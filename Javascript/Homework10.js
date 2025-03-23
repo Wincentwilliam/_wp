6fac079331fb894b6b2a132dd70ed2bf9192e134
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-23 15:52:17 +0800

    commit

---
+++ Javascript/Homework10.js
@@ -0,0 +1,4 @@
+let scores = {Alice:85, Bob:92, Charlie: 78};
+for(let Name in scores) {
+    console.log(`${Name} scores is ${scores[Name]}`);
+}
---
+++ Javascript/Homework9.js
@@ -0,0 +1,4 @@
+let person = {Name:"Alice", Age: 25, City: "Taipei"};
+
+person.Age += 1;
+console.log(person);
