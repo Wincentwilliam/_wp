6c90a3e224781194db2d546813fcca4e2d4b8660
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-28 11:43:08 +0800

    commit

--- Homework/05/Homework5b.js
+++ Homework/05/Homework5b.js
@@ -2,28 +2,43 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
+function page(body) {
+    return `<html>
+  <head>
+  <style>
+  body {
+    background-color:rgb(255, 255, 255);
+  }
+  </style>
+  </head>
+  <body>de
+  ${body}
+  </body>
+  </html>
+  `
+  }
+  
+
 app.use((ctx) => {
   // ctx.response.status = 404
   console.log('url=', ctx.request.url)
   let pathname = ctx.request.url.pathname
   if (pathname == '/') {
-    ctx.response.body = `<html>
-<body>
+    ctx.response.body = page(`
 <h1>我的自我介紹</h1>
 <ol>
 <li><a href="/name">姓名</a></li>
 <li><a href="/age">年齡</a></li>
 <li><a href="/gender">性別</a></li>
 </ol>
-</body>
-</html>
-`
+
+`)
   } else if (pathname == '/name') {
-    ctx.response.body = '洪偉升'
+    ctx.response.body = page('洪偉升')
   } else if (pathname == '/age') {
-    ctx.response.body = '20'
+    ctx.response.body = page('20')
   } else if (pathname == '/gender') {
-     ctx.response.body = '男'
+     ctx.response.body = page('男')
   }
   // ctx.response.body = 'Not Found!'
 });
