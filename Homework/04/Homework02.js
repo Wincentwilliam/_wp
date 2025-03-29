306f038e1549f21c4f410b972dc8b8b5d9b04886
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-29 21:06:42 +0800

    commit

--- Homework/04/Homework02.js
+++ Homework/04/Homework02.js
@@ -1,5 +1,5 @@
 function uniqueSorted(arr) {
-    return [...new set(arr)].sort((a, b) => a - b);
+    return [...new Set(arr)].sort((a, b) => a - b);
 }
 
 console.log(uniqueSorted([5, 3, 8, 3, 1, 5, 8]));
