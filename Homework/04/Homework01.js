7733e1f153d338463353835488a96d372341070c
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-29 20:50:49 +0800

    COMMIT

--- Homework/04/Homework01.js
+++ Homework/04/Homework01.js
@@ -1,25 +1,25 @@
-function countletters(str) {
-    const letterMap = new Map();
+function countLetters(str) {
+    const LetterMap = new Map();
 
     for (const char of str){
         if(/[a-zA-Z]/.test(char)) {
-            const lowerchar = char.tolowercase();
-            letterMap.set(lowerchar, (letterMap.get(lowerchar) || 0) +1);
+            const Lowerchar = char.toLowercase();
+            LetterMap.set(Lowerchar, (LetterMap.get(Lowerchar) || 0) +1);
         }
     }
 
     return letterMap;
 }
 
-function describelettercount(letterMap) {
-    if (letterMap.size === 0) return "I really don't see letter word here!";
+function describeLettercount(LetterMap) {
+    if (LetterMap.size === 0) return "I really don't see letter word here!";
 
     let message = "Here the letter is already breakdown and found:\n";
-    for(const [letter, count]of letterMap) {
-        message += `- '${letter}' appears ${count} time${count > 1 ? 's': ''}.\n`;
+    for(const [Letter, count]of LetterMap) {
+        message += `- '${Letter}' appears ${count} time${count > 1 ? 's': ''}.\n`;
     }
     return message;
 }
 
-console.log(countletters("banana")); //Map {'b' => 1, 'a' => 3, 'n' => 2}
-console.log(describelettercount(countletters("banana")));
+console.log(countLetters("banana")); //Map {'b' => 1, 'a' => 3, 'n' => 2}
+console.log(describeLettercount(countLetters("banana")));
