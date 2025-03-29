c8c8c9574d3a3f3a454c8904fdf12003ddc16c02
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-29 22:38:17 +0800

    commit

--- Homework/04/Homework05.js
+++ Homework/04/Homework05.js
@@ -1,19 +1,13 @@
-function deepMerge(obj1, obj2){
-    for (const key in obj2) {
-        if (obj2.hasOwnProperty(key)) {
-            if (typeof obj2[key] === 'object' && obj2[key] !== null && !Array.isArray(obj2[key])) {
-                if (!obj1[key] || typeof obj1[key] !== 'object' || Array.isArray(obj1[key])) {
-                    obj1[key] = {};
-                }
-                deepMerge(obj1[key], obj2[key]);
-            } else {
-                obj1[key] = obj2[key];
-            }
-        }
+class Animal {
+    constructor(name) {
+        this.name =name;
     }
-    return obj1;
 }
 
-const obj1 = { a: 1, b: { x: 2, y: 3 } };
-const obj2 = { b: { y: 5, z: 6 }, c: 7 };
-console.log(deepMerge(obj1, obj2));
+class dog extends Animal {
+    speak() {
+        return `woof! I am ${this.name}`;
+    }
+}
+const dog = new Dog("Buddy");
+console.log(dog.speak());
