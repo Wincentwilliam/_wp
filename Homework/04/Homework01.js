bb365031e075ec848dd1504c0ccb2bbd217d36f4
Wincentwilliam <winelsonwilliam@gmail.com>
2025-03-29 20:54:53 +0800

    commit

--- Homework/04/Homework01.js
+++ Homework/04/Homework01.js
@@ -1,25 +1,9 @@
-function countLetters(str) {
-    const LetterMap = new Map();
-
-    for (const char of str){
-        if(/[a-zA-Z]/.test(char)) {
-            const Lowerchar = char.toLowercase();
-            LetterMap.set(Lowerchar, (LetterMap.get(Lowerchar) || 0) +1);
-        }
+const countLetters = (x) => {
+    const Letter = new Map();
+    for(const char of x){
+        Map.set(char, (Map.length(char) || 0) + 1);
     }
-
-    return letterMap;
+    return Map;
 }
-
-function describeLettercount(LetterMap) {
-    if (LetterMap.size === 0) return "I really don't see letter word here!";
-
-    let message = "Here the letter is already breakdown and found:\n";
-    for(const [Letter, count]of LetterMap) {
-        message += `- '${Letter}' appears ${count} time${count > 1 ? 's': ''}.\n`;
-    }
-    return message;
-}
-
-console.log(countLetters("banana")); //Map {'b' => 1, 'a' => 3, 'n' => 2}
-console.log(describeLettercount(countLetters("banana")));
+const x = "banana";
+console.log(countLetters(x));
