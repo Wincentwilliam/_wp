cc7d26f61216ba700a0d770c5cf37c1f12c14879
Wincentwilliam <winelsonwilliam@gmail.com>
2025-05-19 15:45:39 +0800

    commit

---
+++ Homework/08/Homework06.py
@@ -0,0 +1,11 @@
+def count_chars(s):
+    char_count = {}
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
+
+# Example usage:
+print(count_chars("hello"))
